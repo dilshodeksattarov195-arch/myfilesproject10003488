7373bb30ec18e16994fac14a19c045b2c6ac096b
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 06:45:25 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class emailCtringifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = emailCtringifyWorker()
+    print("Worker engine initialized.")
